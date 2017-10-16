71ab39dcfc2c93357e94d4dea5141a0a7f65e97a
piyumi_rameshka <rameshkafox@gmail.com>
2017-10-16 15:33:50 +0530

    Query class

---
+++ src/main/java/org/wso2/StrQuery.java
@@ -0,0 +1,34 @@
+package org.wso2;
+
+
+public class StrQuery {
+    String query;
+    String strategy;
+
+
+    public StrQuery(String query, String strategy) {
+        this.query = query;
+        this.strategy = strategy;
+    }
+
+    public StrQuery(String query) {
+        this.query = query;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+
+    public String getStrategy() {
+        return strategy;
+    }
+
+    public void setStrategy(String strategy) {
+        this.strategy = strategy;
+    }
+}
